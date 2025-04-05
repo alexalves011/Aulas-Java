cfb22325ee0affd5f61da1d0f99c5db1f50dec54
alexalves011 <alexalves.work@gmail.com>
2025-04-05 20:37:56 -0300

    Atonomia de Classes

--- trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,10 +1,21 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Ola turma, seja bem-vindos" );
-        
+
+        String primeiroNome = "Alex";
+        String segundoNome = "Alves";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
     }
 
+    public static String nomeCompleto  (String primeiroNome, String SegundoNome){
+        return "Resultado Metodo " + primeiroNome.concat("").concat(SegundoNome);
+
+
+    }
 }
 
 
